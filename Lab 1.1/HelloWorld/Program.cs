1a7d21354c2c24005d877fb127a2576d22cb97d3
Alon Reznik <Arjestin@users.noreply.github.com>
2016-06-13 11:31:05 +0300

    Add files via upload

---
+++ Lab 1.1/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        public static void Main (string[] args)
+        {
+            WriteLine("Hello, C#!");
+            WriteLine("Name: Alon Reznik");
+            WriteLine("Address: Haifa, Israel");
+        }
+    }
+}

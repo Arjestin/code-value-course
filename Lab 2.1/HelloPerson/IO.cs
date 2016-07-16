961d54c5813d6894aefccaf13a41f829519c8bb5
Alon Reznik <Arjestin@users.noreply.github.com>
2016-07-16 17:46:59 +0300

    Add files via upload

---
+++ Lab 2.1/HelloPerson/IO.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HelloPerson
+{
+    internal static class IO
+    {
+        internal static string ReadName()
+        {
+            var name = string.Empty;
+            while (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("What’s your name?");
+                name = Console.ReadLine();
+            }
+            Console.WriteLine($"Hello {name}.");
+            Console.WriteLine();
+            return name;
+        }
+
+        internal static int ReadNumber()
+        {
+            var number = 0;
+            while (number < 1 || number > 10)
+            {
+                var input = string.Empty;
+                while (string.IsNullOrEmpty(input))
+                {
+                    Console.Write("Enter a number in the range 1-10: ");
+                    input = Console.ReadLine();
+                }
+                int.TryParse(input, out number);
+            }
+            return number;
+        }
+
+        internal static void WriteOutput(string name, int number)
+        {
+            for (var i = 0; i < number; ++i)
+            {
+                for (var j = 0; j < i; ++j)
+                {
+                    Console.Write(' ');
+                }
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ Lab 2.1/HelloPerson/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloPerson
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var name = IO.ReadName();
+            var number = IO.ReadNumber();
+            IO.WriteOutput(name, number);
+        }
+    }
+}

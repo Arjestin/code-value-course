84eaa5bef3a3df6ba17d3aaf536f95807a0d0f39
Alon Reznik <Arjestin@users.noreply.github.com>
2016-07-16 17:48:15 +0300

    Add files via upload

---
+++ Lab 2.5/MulBoard/IO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MulBoard
+{
+    internal static class IO
+    {
+        internal static void WriteMulBoard()
+        {
+            for (var row = 1; row <= 10; ++row)
+            {
+                for (var column = 1; column <= 10; ++column)
+                {
+                    Console.Write($"{row*column,4}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Lab 2.5/MulBoard/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MulBoard
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            IO.WriteMulBoard();
+        }
+    }
+}

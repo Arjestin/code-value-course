efc84253a1b3f98d89b7f50be55eceee41118f53
Alon Reznik <Arjestin@users.noreply.github.com>
2016-07-16 17:48:48 +0300

    Add files via upload

---
+++ Lab 2.7/DollarStairs/IO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DollarStairs
+{
+    internal static class IO
+    {
+        internal static uint ReadNumber()
+        {
+            uint number;
+            var input = string.Empty;
+            while (!uint.TryParse(input, out number))
+            {
+                Console.Write("Enter a number of type unsigned integer: ");
+                input = Console.ReadLine();
+            }
+            return number;
+        }
+
+        internal static void WriteDollarStairs(uint number)
+        {
+            for (var i = 0; i <= number; ++i)
+            {
+                for (var j = 0; j < i; ++j)
+                {
+                    Console.Write('$');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Lab 2.7/DollarStairs/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DollarStairs
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var number = IO.ReadNumber();
+            IO.WriteDollarStairs(number);
+        }
+    }
+}

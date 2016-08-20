4254345c96136c70a883afbb9f7f7260f69aa7b4
Alon Reznik <Arjestin@users.noreply.github.com>
2016-08-20 17:22:48 +0300

    Add files via upload

---
+++ Disposable Timer/DelegatesAndEvents/IWorker.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DelegatesAndEvents
+{
+    public interface IWorker : IDisposable
+    {
+        event EventHandler<WorkPerformedEventArgs> WorkPerformed;
+        event EventHandler WorkCompleted;
+
+        void StartWork(WorkType workType, int hours);
+        void StopWork();
+        bool IsWorkCompleted();
+    }
+}
---
+++ Disposable Timer/DelegatesAndEvents/Worker.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Timer = System.Timers.Timer;
+
+namespace DelegatesAndEvents
+{
+    public class Worker : IWorker
+    {
+        private Timer Timer { get; set; }
+        private int HourCount { get; set; }
+        private bool WorkIsCompleted { get; set; }
+        private bool Disposed { get; set; }
+
+        #region Non-Interface Implementation
+
+        public Worker()
+        {
+            WorkIsCompleted = false;
+            Disposed = false;
+        }
+
+        private void TimerHandler(WorkType workType, int hours)
+        {
+            OnWorkPerformed(workType, ++HourCount);
+            if (HourCount != hours)
+            {
+                return;
+            }
+            Timer.Stop();
+            OnWorkCompleted();
+        }
+
+        protected virtual void OnWorkPerformed(WorkType workType, int hours)
+        {
+            WorkPerformed?.Invoke(this, new WorkPerformedEventArgs(workType, hours));
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            WorkIsCompleted = true;
+            WorkCompleted?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (Disposed)
+            {
+                return;
+            }
+            if (disposing && Timer != null)
+            {
+                Timer.Close();
+                Timer.Dispose();
+                Timer = null;
+            }
+            Disposed = true;
+        }
+
+        #endregion
+
+        #region Interface Implementation
+
+        public event EventHandler<WorkPerformedEventArgs> WorkPerformed;
+        public event EventHandler WorkCompleted;
+
+        public void StartWork(WorkType workType, int hours)
+        {
+            HourCount = 0;
+            Timer = new Timer(1000);
+            Timer.Elapsed += (sender, e) => TimerHandler(workType, hours);
+            Timer.Start();
+        }
+
+        public void StopWork()
+        {
+            Timer.Stop();
+        }
+
+        public bool IsWorkCompleted()
+        {
+            return WorkIsCompleted;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
